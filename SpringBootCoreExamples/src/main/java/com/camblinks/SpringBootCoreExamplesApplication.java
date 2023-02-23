2cd3d62311998ff925207be9ffaebca595e834c6
mkumaraj <mkumar.aj@outlook.com>
2023-02-23 07:55:40 -0600

    Initial Commit

---
+++ SpringBootCoreExamples/src/main/java/com/camblinks/SpringBootCoreExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.camblinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreExamplesApplication.class, args);
+	}
+
+}
---
+++ SpringBootCoreExamples/src/test/java/com/camblinks/SpringBootCoreExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camblinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoreExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
